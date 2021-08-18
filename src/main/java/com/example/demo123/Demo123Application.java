c6c08ccdce2c45fae65fc3578d3750879a77ae6e
sangnvhe130388@fpt.edu.vn <sangnvhe130388@fpt.edu.vn>
2021-08-19 00:55:17 +0700

    update

--- src/main/java/com/example/demo123/Demo123Application.java
+++ src/main/java/com/example/demo123/Demo123Application.java
@@ -1,11 +1,14 @@
 package com.example.demo123;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class Demo123Application {
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo123/repository/DocumentRepository.java
+++ src/main/java/com/example/demo123/repository/DocumentRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 public interface DocumentRepository  extends JpaRepository<document, Long> {
     @Query(value = "SELECT * FROM rts.document where course_id = ?;",nativeQuery = true)
     List<document> getAllByCourse(Long courseId);
-    @Query(value = "SELECT a.id,a.created_date,a.created_by,a.title,a.security,a.course_id,b.title as course_name,a.document_name,c.id as category_id,c.name as category_name FROM rts.document a inner join rts.courses b on a.course_id =b.id inner join rts.categories c on b.category_id = c.id where a.security like 'public'",nativeQuery = true)
+    @Query(value = "SELECT a.id,a.created_date,a.created_by,a.title,a.security,a.course_id,b.title as course_name,a.document_name,c.id as category_id,c.name as category_name FROM rts.document a inner join rts.courses b on a.course_id =b.id inner join rts.categories c on b.category_id = c.id where a.security like 'public' and b.status like 'active'",nativeQuery = true)
     List<documentRespon> getAllBySecurity();
 }
